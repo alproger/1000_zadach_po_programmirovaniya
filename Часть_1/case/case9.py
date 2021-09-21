88930c296d964bd21d49e7c0e82b33ee4b609328
Kodirov Yakhyokhon <90234997+alproger@users.noreply.github.com>
2021-09-21 06:57:36 +0500

    case tasks

--- Часть_1/case/case9.py
+++ Часть_1/case/case9.py
@@ -3,45 +3,44 @@ month = int(input('Вводите число  месяца : '))
 
 #1
 if month in [1, 3, 5, 7, 8, 10, 12] :
-    if day == 1 :
-        day, month  = 30, month -1
-        print('{}.{}'.format(day, month))
-
-        if month == 1 :
-            day, month = 31, 12
-            print('{}.{}'.format(day, month))
+    if day == 31 :
+        day, month = 1, month + 1,
         
-        elif month == 3 :
-            day, month = 28, 2
+        if month == 12 :
+            month, day = 1, 1
             print('{}.{}'.format(day, month))
-   
+
+        print('{}.{}'.format(day, month))
+
     elif day > 31 :
         print('Такой даты нет ')
-
-    else :
-        day -= 1
-        print('{}.{}'.format(day, month))
     
-elif month == 2 :
-    if day == 1 :
-        day, month = 31, 1
+    else :
+        day += 1
         print('{}.{}'.format(day, month))
 
-    elif day > 31 :
+#2    
+elif month == 2 :
+    if day == 28 :
+        day, month = 1, month + 1   
+        print('{}.{}'.format(day, month))      
+
+    elif day > 28 :
         print('Такой даты нет ')
 
     else :
-        day -= 1
+        day += 1
         print('{}.{}'.format(day, month))
 
+#3
 elif month in [4, 6, 9, 11] :
-    if day == 1 : 
-        day, month = 31, month - 1
+    if day == 30 : 
+        day, month = 1, month + 1
         print('{}.{}'.format(day, month))
 
     elif day > 30 : 
         print('Такой даты нет ')
 
     else :
-        day -= 1
+        day += 1
         print('{}.{}'.format(day, month))
